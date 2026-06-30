9606cbf47f2bb522da8755b118a623ab6b8bcf5a
wakeshinidie <wowchild233@gmail.com>
2026-06-30 23:57:39 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // 强行忽略所有代码路径和类型错误
     ignoreBuildErrors: true,
   },
   eslint: {
+    // 强行忽略所有规范检查
     ignoreDuringBuilds: true,
   },
 }
 
-module.exports = nextConfig
+// 换成现代的导出方式，防止引擎直接挂掉
+export default nextConfig
